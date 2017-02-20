8596a7ba3b7f86ab367022f7bc6f8f0816a14af1
Juan Ortiz <13-11021@usb.ve>
2017-02-20 16:40:35 -0400

    Initial Commit

---
+++ CPMA/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CPMA
+//
+//  Created by Juan Tepedino on 20/2/17.
+//  Copyright © 2017 Juan Tepedino. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
